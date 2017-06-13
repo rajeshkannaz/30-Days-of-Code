6aa0dd11630e6da300f33a0ea0f34be1ca9cd7f5
rajeshkannaz <rajeshravi3366@gmail.com>
2017-06-13 13:11:29 +0530

    Create Solution.py

--- 0 - Hello, World./Solution.py
+++ 0 - Hello, World./Solution.py
@@ -1,5 +1,5 @@
 # Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
-input_string = input()
+input_string = raw_input()
 
 # Print a string literal saying "Hello, World." to stdout.
 print('Hello, World.')
